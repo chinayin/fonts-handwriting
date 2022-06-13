f935fa9a0bdbf252623e5a3becfc603da13d37e9
lei.tian <whereismoney@qq.com>
2022-06-13 19:32:24 +0800

    增加2款字体

--- src/FontsHandwriting.php
+++ src/FontsHandwriting.php
@@ -4,20 +4,34 @@ namespace Fonts;
 
 class FontsHandwriting
 {
+    private static $mapping = [
+        'WQSXT' => '王强手写体',
+        'MUYAO-SOFTBRUSH-2' => '沐瑶软笔手写体',
+        'CYHYBXS' => '蔡云汉硬笔行书简书法',
+    ];
 
     public static function getPath(): string
     {
         return self::useRandom();
     }
 
-    public static function all():array{
+    public static function getFont(string $key): array
+    {
+        return [
+            'name' => self::$mapping[$key] ?? '',
+            'path' => __DIR__ . '/../assets/' . $key . 'ttf',
+        ];
+    }
+
+    public static function all(): array
+    {
         return glob(__DIR__ . '/../assets/*.[tT][tT][fF]');
     }
 
     public static function useRandom(): string
     {
         $list = self::all();
-        return $list[array_rand($list)] ;
+        return $list[array_rand($list)];
     }
 
 }
